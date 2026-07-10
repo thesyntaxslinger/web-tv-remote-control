c476c31839550c16764c192b0a36840e7c18d4e9
thesyntaxslinger <github@syntaxslinger.com>
2026-07-10 18:10:37 +1000

    fix: cleanup shebangs

--- src/web_tv_remote_control/__main__.py
+++ src/web_tv_remote_control/__main__.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from .webserver import main
 
 if __name__ == '__main__':
--- src/web_tv_remote_control/keys.py
+++ src/web_tv_remote_control/keys.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from evdev import UInput, ecodes as e
 
 # make allowed keys
