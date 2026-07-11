25de763f705318aa4ff2614a3aba7edb3b8ed16e
thesyntaxslinger <github@syntaxslinger.com>
2026-07-11 20:29:16 +1000

    fix: supress entrypoint permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-addgroup -g "$PGID" appuser
-adduser -D -u "$PUID" -G appuser appuser
+addgroup -g "$PGID" appuser >/dev/null 2>&1
+adduser -D -u "$PUID" -G appuser appuser >/dev/null 2>&1
 
-chown appuser:appuser /dev/uinput
+chown appuser:appuser /dev/uinput >/dev/null 2>&1 
 
 exec su-exec appuser "$@"
