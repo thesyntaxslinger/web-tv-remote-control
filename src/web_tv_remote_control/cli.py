0e3498ecd144deb1f75fb86b79d60c7b17c8abef
thesyntaxslinger <github@syntaxslinger.com>
2026-07-11 17:30:50 +1000

    fix: formatting for cli.py

--- src/web_tv_remote_control/cli.py
+++ src/web_tv_remote_control/cli.py
@@ -1,10 +1,11 @@
 import argparse
+import os
 
 def make_parser():
     parser = argparse.ArgumentParser(
         prog='web-tv-remote-control',
         description='A web-ui to control your PC with a TV remote from another device.'
     )
-    parser.add_argument('-H', '--host', default='::', type=str, help='specifies the host to listen on')
-    parser.add_argument('-p', '--port', default=8080, type=int, help='specifies the port to start the webserver with')
+    parser.add_argument('-H', '--host', default=os.environ.get('HOST', '::'), type=str, help='specifies the host to listen on')
+    parser.add_argument('-p', '--port', default=os.environ.get('PORT', 8080), type=int, help='specifies the port to start the webserver with')
     return parser
